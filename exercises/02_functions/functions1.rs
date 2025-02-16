9a28058c3984e5acc10f6eb144638a8af5d9d07e
Samuel Huang <samhuang91@gmail.com>
2025-02-16 14:54:38 -0800

    Complete 02_functions

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,5 +1,10 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
 
+fn call_me() {
+    let name: &str = "Sam";
+    println!("Hello {name}! How are you doing?");
+}
+
 fn main() {
     call_me(); // Don't change this line
 }
