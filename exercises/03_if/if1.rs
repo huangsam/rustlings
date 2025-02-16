3c0591ca029cd710a152bbb7f8671db379688513
Samuel Huang <samhuang91@gmail.com>
2025-02-16 15:11:06 -0800

    Complete 03_if

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,15 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b { a } else { b }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let a: i32 = 1;
+    let b: i32 = 3;
+    let val: i32 = bigger(a, b);
+    println!("The bigger value between {a} and {b} is {val}");
 }
 
 // Don't mind this for now :)
